b32c32755977417c261ac4b5e46aecc75c0666c3
petous22 <35193004+petous22@users.noreply.github.com>
2022-06-19 15:21:02 +0200

    Add files via upload

---
+++ arduino_secrets - kopie.h
@@ -0,0 +1,10 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "...yourwifi..."
+#define SECRET_PASS "...your wifi password"
+
+//ThingSpeak
+#define SECRET_CH_ID 0000000      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "xyz"   // replace XYZ with your channel write API Key
+#define SECRET_READ_APIKEY "xyz"   // replace XYZ with your channel write API Key
